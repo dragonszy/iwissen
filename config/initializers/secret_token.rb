702a899c63d1de765d892969f7697eabc19841ba
dragonszy <dragonszy@gmail.com>
2013-05-26 15:11:14 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwissen::Application.config.secret_token = 'b5706ec5ee4ac9959243c317fabfcacf2dfac496648546e2ac23c0856de5db69017c0a9b08880897aed8ad52a5489d2dc5a1e5c130d49656a6a583aaace2a317'
