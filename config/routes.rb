21e76368afe0322535fadc4049ab3a4309dd6122
dragonszy <dragonszy@gmail.com>
2013-05-26 22:25:26 +0800

    StaticPages controller route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Iwissen::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/about"
-
-  get "static_pages/help"
+  match '/' => 'static_pages#home'
+  match '/help' => 'static_pages#help'
+  match '/about' => 'static_pages#about'
 
   authenticated :user do
-    root :to => 'home#index'
+    root :to => "static_pages#home"
   end
   root :to => "home#index"
   devise_for :users
