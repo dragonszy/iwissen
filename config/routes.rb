dc8ab23591870eea1ca086ddeb41b4da5fcedad5
dragonszy <dragonszy@gmail.com>
2013-05-26 22:02:45 +0800

    add StaticPages controller home about help action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Iwissen::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/help"
+
   authenticated :user do
     root :to => 'home#index'
   end
