a3e23aac5b5f04e5e8b947194b2a98753e8517d6
dragonszy <dragonszy@gmail.com>
2013-05-27 10:41:39 +0800

    rspec title test has some problems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,13 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def full_title(page_title)
+    base_title = '爱知学堂'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,46 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "HomePage (GET /)" do
-    it "shoud have the content '首页 | 爱知学堂'" do
+    it "shoud have the title '首页 | 爱知学堂'" do
       visit '/'
-      page.should have_content('首页 | 爱知学堂')
+      page.should have_selector('title',
+      						:text => '首页 | 爱知学堂')
+    end
+    it "shoud have the h1 '爱知学堂'" do
+      visit '/'
+      page.should have_selector('h1',
+      						:text => '爱知学堂')
     end
   end
+
+  describe "HelpPage (GET /help)" do
+  	it "should have the title '帮助 | 爱知学堂'" do
+  	  visit '/help'
+  	  page.should have_selector('title',
+      						:text => '帮助 | 爱知学堂')
+  	end
+  	it "should have the h1 '帮助'" do
+  	  visit '/help'
+  	  page.should have_selector('h1',
+      						:text => '帮助')
+  	end
+  end
+
+
+  describe "AboutPage (GET /about)" do
+
+  	it "should have the title '关于我们 | 爱知学堂'" do
+  	  visit '/about'
+  	  page.should have_selector('title',
+      						:text => '关于我们 | 爱知学堂')
+  	end
+
+  	it "should have the h1 '关于我们'" do
+  	  visit '/about'
+  	  page.should have_selector('h1',
+  	  						:text => '关于我们')
+  	end
+
+  end
+
 end
