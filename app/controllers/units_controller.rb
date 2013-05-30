ab39a56c61dfb3f09a995408366676c298b1e437
dragonszy <dragonszy@gmail.com>
2013-05-30 12:21:34 +0800

    course#show can add units, but errors msg occur twice, need to fix

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,7 +1,7 @@
 class UnitsController < ApplicationController
 
   def index
-  	
+  	@units = Unit.all
   end
 
   def show
@@ -18,9 +18,12 @@ class UnitsController < ApplicationController
     @unit = Unit.new(params[:unit])
 
     if @unit.save
-      redirect_to "/courses/#{@course.id}"
+      # redirect_to :back may work properly, but is it the best practice?
+      redirect_to :back
+      # redirect_to "/courses/#{@course.id}"
+      # some errors in redirection
     else
-
+      redirect_to :back, :notice => "未能成功创建课程单元"
     end
   end
 
