2feb2297a8dfdbdfe60283eb6f36e2ab1461377c
dragonszy <dragonszy@gmail.com>
2013-06-02 21:26:31 +0800

    change route and some pic

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Iwissen::Application.routes.draw do
     resources :lessons
   end
 
-
   match '/' => 'static_pages#home'
   match '/help' => 'static_pages#help'
   match '/about' => 'static_pages#about'
 
   authenticated :user do
-    root :to => "static_pages#home"
+    root :to => 'static_pages#home'
   end
-  root :to => "home#index"
+
+  root :to => 'static_pages#home'
   devise_for :users
   resources :users
 end
